ec4bcb2f6568a786920f199468e7f09120213a2a
GuitarOTronic <seanobringer90@gmail.com>
2019-12-07 10:42:40 -0700

    adds socket io

--- src/App.js
+++ src/App.js
@@ -1,10 +1,16 @@
 import React, { Component } from 'react';
 import Game from './Components/GameSetup/Game'
+import openSocket from 'socket.io-client';
 import './stylesheet.css'
 
 class App extends Component {
 
   render(){
+    const socket = openSocket('http://localhost:8080');
+    socket.emit('goal', 'heeeey');
+    socket.on('sup', (data) => {
+    console.log(data);
+  });
     return (
        <main className="column main-container" id="main">
           <Game />
